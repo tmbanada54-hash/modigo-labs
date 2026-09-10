6ff01db10aaa9591f24e6abba1f10b1d6d9cc1b0
tmbanada54-hash <praisecrystal94@gmail.com>
2026-09-10 11:45:05 +0100

    Complete: Common Interests Finder

--- challenges/main.py
+++ challenges/main.py
@@ -1,3 +1,8 @@
-def merge_tags(tags1, tags2):
-    merged = tags1 | tags2
-    return merged
+def compare_hobbies(person1_hobbies, person2_hobbies):
+    return{
+        "shared": person1_hobbies & person2_hobbies,
+        "only_person1": person1_hobbies - person2_hobbies,
+        "only_person2": person2_hobbies - person1_hobbies
+    }
+    # TODO: use set operations to find shared, only_person1, and only_person2 hobbies
+    pass

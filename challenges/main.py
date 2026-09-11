9c74106bcd228424ea881e44c09c89b6d30a7679
tmbanada54-hash <praisecrystal94@gmail.com>
2026-09-11 09:56:01 +0100

    Complete: BMI Category Report

--- challenges/main.py
+++ challenges/main.py
@@ -1,7 +1,16 @@
-def path_hits_blocked(blocked, path):
-    for block in blocked:
-        if block in path:
-            return True
-    return False 
-    # TODO: check whether any position in `path` also appears in `blocked`
+def bmi_report(weight_kg, height_m):
+    bmi = weight_kg / (height_m ** 2)
+    bmi = round(bmi, 1)
+    if bmi < 18.5:
+        category = "Underweight"
+    elif bmi <= 24.9:
+        category =  "Normal weight"
+    elif bmi <= 29.9:
+        category = "Overweight"
+    else:
+        category = "Obese"      
+        
+    return(f"BMI: {bmi}, Category: {category}")            
+    # TODO: calculate bmi, round it to 1 decimal place, determine the category,
+    # and return "BMI: {bmi}, Category: {category}"
     pass

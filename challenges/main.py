b9d8fb29937ce6ade69bef6ce0ea0d7c25a091fd
tmbanada54-hash <praisecrystal94@gmail.com>
2026-09-23 08:31:28 +0100

    Complete: Fix the Discount Applier

--- challenges/main.py
+++ challenges/main.py
@@ -1,11 +1,2 @@
-def has_conflict(meetings):
-    for tuaka in range(len(meetings)):
-        start1, end1 = meetings[tuaka]
-        for praise in range(tuaka + 1, len(meetings)):
-            start2, end2 = meetings[praise]
-            if start1 < end2 and start2 < end1:
-                return True
-    return False            
-
-    # TODO: return True if any two meetings overlap in time, False otherwise
-    pass
+def apply_discount(price, discount_percent=10):
+    return round(price - (price * discount_percent / 100), 2)
